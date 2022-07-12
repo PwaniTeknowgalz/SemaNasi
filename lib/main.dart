f3f7e81d5c10dc736b558dd60d06768ffe461771
Badru254 <suleybadr@yahoo.com>
2022-07-12 08:27:07 +0300

    Changed db key

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
-import 'package:semanasi/frontend/screens/dashboard.dart';
 import 'backend/main_binding.dart';
 import 'frontend/auth/login.dart';
-import 'frontend/screens/dash.dart';
+import 'frontend/screens/dashboard.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  //Actual Credentials
   var keyParseApplicationId = "T3AOj3qSiFh0Y8l2jF8Li6Vb4smGJQ8rYb01McIX";
-  var keyParseServerUrl = "https://parseapi.back4app.com";
   var keyParseClientKey = "CpA87GrXhF66n1YHtdpgDWpIboGLcSKq2CQH8edQ";
 
-  var store = await CoreStoreSembastImp.getInstance(password: "mobipeace");
+  //Test Credentials
+  // var keyParseApplicationId = "8G0QcO27nAbdnCMIU3VGMdDVt86off4DmHkFFAGg";
+  // var keyParseClientKey = "4Y1iXxtkfiWxEAHK9SG6lUZjnqp88gRt2rThFVm5";
+
+  var keyParseServerUrl = "https://parseapi.back4app.com";
+
+  var store = await CoreStoreSembastImp.getInstance(password: "semanasi");
 
   await Parse().initialize(keyParseApplicationId, keyParseServerUrl,
       clientKey: keyParseClientKey, autoSendSessionId: true, coreStore: store);
